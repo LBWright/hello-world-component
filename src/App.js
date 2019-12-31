2f1a3e0d47b6ae1b3d5acc1fd9af7dc1a484eae4
Logan Wright <loganwright@Logans-MacBook-Pro.local>
2019-12-31 13:31:47 -0600

    updates webpack and adds user props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(props) {
+  console.log("props from inside of App.js");
+  console.log({ user: props.user.toJS() });
+  const user = props.user.toJS();
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{`Hello ${user.first_name}. Your email is ${user.email}`}</p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import App from "./App";
 
+console.info("preparing to render component");
+window.customComponentIsReady = true;
+
 export default App;
